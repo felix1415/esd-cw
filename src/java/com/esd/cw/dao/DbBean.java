331b7e1eb0651e4d375d22b74c2259c5a1afdabf
= <aidanayala-angear@aidans-mbp>
2016-10-27 09:17:50 +0100

    Couple of TODO's & formatting

--- src/java/com/esd/cw/dao/DbBean.java
+++ src/java/com/esd/cw/dao/DbBean.java
@@ -5,7 +5,7 @@
  */
 package com.esd.cw.dao;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource; // TODO this errors for us unless we download. We need to ask if markers expect to be able to just fork and run this code. Or if they are prepared to install a few libs to get it working.
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -17,42 +17,41 @@ import java.sql.Statement;
  * @author shaun
  */
 public class DbBean {
-    
-  private Connection con;
-  private Statement state;
-  private ResultSet rs;
-  
-  public DbBean(String url, String username, String password) throws SQLException, InstantiationException{
-      try {
-      // You will need to explicitly load this driver in a web app
-      MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUser(username);
-        dataSource.setPassword(password);
-        dataSource.setServerName(url);
-        dataSource.setDatabaseName("esd");
-      
-      con = dataSource.getConnection();
-      
-    } catch (SQLException e) {
-      System.err.println("Error: " + e);
 
+    private Connection con;
+    private Statement state;
+    private ResultSet rs;
+
+    public DbBean(String url, String username, String password) throws SQLException, InstantiationException {
+        try {
+            // You will need to explicitly load this driver in a web app
+            MysqlDataSource dataSource = new MysqlDataSource();
+            dataSource.setUser(username);
+            dataSource.setPassword(password);
+            dataSource.setServerName(url);
+            dataSource.setDatabaseName("esd"); // TODO shouldn't hardcode this. Should set as a context attribute? (or whichever is relevant)
+
+            con = dataSource.getConnection();
+
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+
+        }
     }
-  }
 
-  public String doQuery(String query) throws SQLException{
-      StringBuilder sb = new StringBuilder();
-      state = con.createStatement();
-      rs = state.executeQuery(query);
-      while (rs.next()) {
-        sb.append(rs.getString(1) + " " + rs.getString(2));
-        sb.append("\n<br>");
-        System.out.println(rs.getString(1) + " " + rs.getString(2));
-      }
-      rs.close();
-      state.close();
-      con.close();
-      return sb.toString();
-  }
-  
+    public String doQuery(String query) throws SQLException {
+        StringBuilder sb = new StringBuilder();
+        state = con.createStatement();
+        rs = state.executeQuery(query);
+        while (rs.next()) {
+            sb.append(rs.getString(1) + " " + rs.getString(2));
+            sb.append("\n<br>");
+            System.out.println(rs.getString(1) + " " + rs.getString(2));
+        }
+        rs.close();
+        state.close();
+        con.close();
+        return sb.toString();
+    }
+
 }
-
