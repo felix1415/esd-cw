52860337cd0c81356932eca9621f2bf871c9c0e0
shaunlee0 <shaunlee0@googlemail.com>
2016-11-14 17:44:38 +0000

    Add address web service integration.

---
+++ src/java/com/esd/cw/services/AddressLookupService.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esd.cw.services;
+
+import com.esd.cw.util.PropertiesUtil;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+import sun.net.www.http.HttpClient;
+
+/**
+ *
+ * @author shaun
+ */
+public class AddressLookupService {
+    
+    public String doLookup(String postcode) throws MalformedURLException, IOException{
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        String username = PropertiesUtil.getPropertyAsString("address-lookup-api-username");
+        String password = PropertiesUtil.getPropertyAsString("address-lookup-api-password");
+        String urlString = PropertiesUtil.getPropertyAsString("address-lookup-api-url") + postcode;
+        
+     
+        
+        URL url = new URL (urlString);
+        String encodingString = username + ":" + password;
+        String encoding = Base64.getEncoder().encodeToString(encodingString.getBytes());
+
+            URLConnection connection =  url.openConnection();
+           
+            connection.setRequestProperty  ("Authorization", "Basic " + encoding);
+            InputStream content = connection.getInputStream();
+            BufferedReader in   = 
+                new BufferedReader (new InputStreamReader (content));
+            String line;
+            while ((line = in.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            
+          
+        
+        return stringBuilder.toString().replace(" , , , , ", "");
+    }
+    
+}
