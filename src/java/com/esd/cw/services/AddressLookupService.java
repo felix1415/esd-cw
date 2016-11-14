e163d70a713cf12c66921f4a28424f6776b8ef6d
felix1415 <alexgray1415@gmail.com>
2016-11-14 17:57:30 +0000

    Removed unused imports from missing libraries not used.

--- src/java/com/esd/cw/services/AddressLookupService.java
+++ src/java/com/esd/cw/services/AddressLookupService.java
@@ -10,17 +10,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Base64;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.DefaultHttpClient;
-import sun.net.www.http.HttpClient;
 
 /**
  *
